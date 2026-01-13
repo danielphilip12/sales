323da3194e18e4d06d30e2616407ab326652c5d4
Daniel <daniel.gallo12@gmail.com>
2026-01-12 16:22:02 -0800

    initial setup code

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+st.title("Hi there")
+
+# --- Pages Section Setup ---
+
+about_page = st.Page(
+    "templates/about.py",
+    title="About me",
+    icon=":material/account_circle:",
+    default=True,
+)
+
+project_1_page = st.Page(
+    "templates/dashboard.py", title="Sales Dashboard", icon=":material/bar_chart:"
+)
+
+# --- Navigation Session Setup ---
+pn = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page],
+    }
+)
+
+
+# --- Running Navigation ---
+pn.run()
